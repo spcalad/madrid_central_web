862b27acbd46f44dc66126ce908b0e567c1fa0fd
Sandra Cala <spcalad@gmail.com>
2020-04-03 19:05:28 +0200

    try one transit stations

--- app/controllers/stations_controller.py
+++ app/controllers/stations_controller.py
@@ -8,12 +8,12 @@ def index_stations():
     return render_template('stations/index.html', stations=stations)
 
 def create_stations():
-    stations = Station.read_stations_file(request.files['file'])
+    stations = Station.read_stations_file(request.files['file'], request.form.get('stationCategory'))
     for station in stations:
         new_station = Station(id=station[0], name=station[1], type=station[2],
                               address=station[3], latitude=station[4],
                               longitude=station[5], altitude=station[6],
-                              start_date=station[7])
+                              start_date=station[7], category=station[8])
         db.session.add(new_station)
         db.session.commit()
     return redirect('/stations')
