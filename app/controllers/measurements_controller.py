62ad36687c77a6e702a7a2c5778648b37f2dc81e
Andres Plazas <acpr87@gmail.com>
2020-04-04 18:16:32 +0200

    Se modifican el controlador de la vista measurement. Se actualiza el archivo __init__ de controladores

--- app/controllers/measurements_controller.py
+++ app/controllers/measurements_controller.py
@@ -1,5 +1,34 @@
-from flask import render_template
+from flask import render_template, request, redirect
+from app.models.measurement import Measurement, FileReader
+from app import db
 
-#@measurements_blueprints.route('/')
+
+# @measurements_blueprints.route('/')
 def index_measurements():
-    return render_template('measurements/index.html')
+    measurements = Measurement.query
+    return render_template('measurements/index.html', measurements=measurements)
+
+
+def create_measurements():
+    measurement_obj = FileReader()
+    measurement_obj.read_measurement_file(request.files['file'])
+
+    measurements = []
+    for measurement in measurement_obj.maintable:
+        measurements.append(Measurement(
+            station_id=measurement[0],
+            day_id=measurement[1],
+            time_id=measurement[2],
+            magnitude_id=measurement[3],
+            value=measurement[4],
+            validation=measurement[5]
+        ))
+
+    db.session.add_all(measurements)
+    db.session.commit()
+
+    return redirect('/measurements')
+
+
+def delete_measurements():
+    pass
